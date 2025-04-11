2ae05a7ce65e9fa9f0ee76a71efe09dcbedc75bb
Waleed Ashraf <102048956+waleed750@users.noreply.github.com>
2025-04-11 15:42:28 +0200

    update Android Gradle plugin version to 8.2.1

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import ffmpeg_kit_flutter_min_gpl
+import file_picker
 import path_provider_foundation
 import sqflite_darwin
 import video_compress
@@ -13,6 +14,7 @@ import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   VideoCompressPlugin.register(with: registry.registrar(forPlugin: "VideoCompressPlugin"))
